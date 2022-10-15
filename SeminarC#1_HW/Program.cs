a2b894d4d85168c755a0ccbcadbaa167bfcc9357
Natalya Vaida <naelia.ru@gmail.com>
2022-10-15 19:07:15 +0300

    add 4 task

--- SeminarC#1_HW/Program.cs
+++ SeminarC#1_HW/Program.cs
@@ -54,7 +54,7 @@ Console.Write("Max number is " + max);
 
 4 -> да
 -3 -> нет
-7 -> нет*/
+7 -> нет
 
 Console.Write("Input your number ");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -64,4 +64,30 @@ int result = num % 2;
 if (result == 0)
 Console.Write("Your number is even");
 else 
-Console.Write("Your number is odd");
+Console.Write("Your number is odd");*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Input a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+if (num < 0)
+{
+    current = num;
+    num = -num;
+}
+
+while (current <= num)
+{
+    if (current % 2 == 0)
+    { 
+        Console.Write(current + " ");
+        current++;
+    }
+    else current++;
+}
