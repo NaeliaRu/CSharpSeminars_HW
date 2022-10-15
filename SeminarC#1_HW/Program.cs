f66ed86ce1c29e1193ea3178fa38015825d2e8a5
Natalya Vaida <naelia.ru@gmail.com>
2022-10-15 17:32:34 +0300

    add 2 task

--- SeminarC#1_HW/Program.cs
+++ SeminarC#1_HW/Program.cs
@@ -4,7 +4,7 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Input a first number: ");
+/*Console.Write("Input a first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -21,4 +21,25 @@ else
     }
     else 
     Console.Write($"Number {b} bigger then number {a}");
-}
+}*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("Input first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("Max number is " + max);
