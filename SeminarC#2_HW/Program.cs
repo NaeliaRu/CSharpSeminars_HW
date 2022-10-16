a7b2ec79c395e46604c49de7f40f55da430e9c09
Natalya Vaida <naelia.ru@gmail.com>
2022-10-16 19:14:06 +0300

    SeminarC#2 HW added

---
+++ SeminarC#2_HW/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int FindSecondDigit(int num)
+{
+    int result = num % 100 /10;
+    return result;
+}
+
+Console.Write("Input a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = FindSecondDigit(number);
+Console.WriteLine($"Second digit of {number} is {secondDigit}");
+
+
+
