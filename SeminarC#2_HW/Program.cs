62391b03ac2d26f1dc6310de165ea67dd9cf582e
Natalya Vaida <naelia.ru@gmail.com>
2022-10-16 21:57:00 +0300

    seminarC# HW second task added

--- SeminarC#2_HW/Program.cs
+++ SeminarC#2_HW/Program.cs
@@ -2,7 +2,7 @@
 
 456 -> 5
 782 -> 8
-918 -> 1 */
+918 -> 1
 
 int FindSecondDigit(int num)
 {
@@ -14,7 +14,41 @@ Console.Write("Input a three-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int secondDigit = FindSecondDigit(number);
-Console.WriteLine($"Second digit of {number} is {secondDigit}");
+Console.WriteLine($"Second digit of {number} is {secondDigit}"); */
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 
+
+int FindThirdDigit(int num)
+{
+    int result;
+
+    if (num < 100)
+    return -1;
+
+    else
+    {
+        while (num >= 1000) 
+        {
+            num = num / 10;
+        }
+        result = num % 10;
+    }
+    return result;
+}
+
+Console.Write("Input a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = FindThirdDigit(number);
+Console.Write($"Third digit of {number} is {thirdDigit}"); */
+
+
+
+
+
 
 
 
