4e398d541c1ba937986247420834397595c62bd0
Natalya Vaida <naelia.ru@gmail.com>
2022-10-17 22:54:04 +0300

    seminarc#_hw 2 task added

--- SeminarC#3_HW/Program.cs
+++ SeminarC#3_HW/Program.cs
@@ -22,3 +22,34 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 bool result = Polindrom(number);
 Console.WriteLine($"Your number is a palindrome {result}"); */
+
+/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+/*double FindDistans(double x, double y, double z, double x1, double y1, double z1)
+{
+    double num = 0;
+    num = Math.Pow((x1 - x), 2) + Math.Pow((y1 - y), 2) + Math.Pow((z1 - z), 2);
+    num = Math.Sqrt(num);
+    num = Math.Round(num, 2);
+    return num;
+}
+
+Console.Write("Input x coordinate of the first point: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y coordinate of the first point: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input z coordinate of the first point: ");
+double z = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input x coordinate of the second point: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y coordinate of the second point: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input z coordinate of the second point: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+double findDistans = FindDistans(x, y, z, x1, y1, z1);
+Console.WriteLine($"Distans between your two point is {findDistans}");*/
+
+
