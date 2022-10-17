152378f23861a6ebac54dda1cd605d6591f9e7a6
Natalya Vaida <naelia.ru@gmail.com>
2022-10-17 22:27:37 +0300

    seminarc#_hw 1 task added

---
+++ SeminarC#3_HW/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+
+bool Polindrom(int num)
+{
+    int num1 = num / 10000;
+    int num2 = num / 1000 % 10;
+    int num4 = num % 100 / 10;
+    int num5 = num % 10;
+
+    if(num1 == num5 && num2 == num4)
+    {
+        return true;
+    }
+    return false;
+}
+
+Console.Write("Input five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Polindrom(number);
+Console.WriteLine($"Your number is a palindrome {result}"); */
