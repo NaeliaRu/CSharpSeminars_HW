036e5763ae115fd759514cdc3baf0d3eb326a9a2
Natalya Vaida <naelia.ru@gmail.com>
2022-11-07 23:42:05 +0300

    SeminarC#7 home work exercise 50 added

--- SeminarC#7_HW/Program.cs
+++ SeminarC#7_HW/Program.cs
@@ -4,7 +4,7 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
 
-double[,] CreateRandom2dArray()
+/* double[,] CreateRandom2dArray()
 {
     Console.Write("Input a number of rows: ");
     int rows = Convert.ToInt32(Console.ReadLine());
@@ -39,6 +39,59 @@ void Show2dArray(double[,] array)
 }
 
 double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray); */
+
+/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> числа с такими индексами в массиве нет */
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of colums: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows,colums];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+    }
+return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+
+void FindUserElement(int[,] array)
+{
+    Console.Write("Input the position of the element in the row: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input the position of the element in the colum: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+
+    if(i < array.GetLength(0) && j < array.GetLength(1))
+        Console.Write("Your element is: " + array[i,j]);
+    else Console.Write("No such element in array");
+}
+
+int[,] myArray = CreateRandom2dArray();
 Show2dArray(myArray);
-
-
+FindUserElement(myArray);
